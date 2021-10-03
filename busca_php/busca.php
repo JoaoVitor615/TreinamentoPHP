838e34c8ebe2e63f2c1bcf8de423c24a68be7e3c
João Vítor <joao.peniza@etec.sp.gov.br>
2021-10-03 16:28:51 -0300

    implementando o mecanismo de busca

--- busca_php/busca.php
+++ busca_php/busca.php
@@ -10,9 +10,53 @@ if(!isset($_GET['nome_livro'])){
 /*
 O método trim() remove os espaços em branco (whitespaces) do início
 e/ou fim de um texto. É considerado espaço em branco (espaço, tabulação, 
-espaço fixo/rígido, etc.) e todo sinal de fim de linha de texto.
+espaço fixo/rígido, etc.) e todo sinal de fim de linha de texto;
 
-O "%" serve para no banco de dados 
+O % serve para pesquisas no banco de dados;
+
+O . é para concatenar.
 */
 
+$nome = "%".trim($_GET['nome_livro'])."%";
+
+/*
+
+O prepare() vai servir para fazer o mesmo que uma query no banco de dados,
+porém não serão valores estáticos, pois é possível executar(execute()) o prepare 
+varias vezes porém injetando valores diferentes a cada vez;
+
+O bindParam() vai passar os parâmetros para o a query que vai ser executada no 
+banco de dados.
+
+*/
+$dbh = new PDO('mysql:host=Localhost; dbname=db_buscaphp', 'root', '06012005');
+$sth = $dbh->prepare('select * from `tbl_livro` where `nome` like :nome');
+$sth->bindParam(':nome', $nome, PDO::PARAM_STR);
+$sth->execute();
+
+$resultados = $sth->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($resultados); exit;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado da Busca</title>
+</head>
+<body>
+    <h2>Resultado da Busca</h2>
+    <?php
+    if(count($resultados)) {
+        foreach($resultados as $Resultado){
+    ?>
+    <label><?php echo $Resultado['nome']; ?></label>
+    <br>
+    <?php
+    }} else { ?>
+    <label>Não foram encontrados resultados pelo termo buscado</label>
+    <?php }?>
+</body>
+</html>
--- busca_php/db_buscaphp.sql
+++ busca_php/db_buscaphp.sql
@@ -1,5 +1,5 @@
 create database db_buscaphp;
-
+select * from tbl_livro where nome like '%code%';
 use db_buscaphp;
 
 create table tbl_livro (
@@ -10,3 +10,11 @@ nome varchar(300) not null
 insert into tbl_livro (nome) values ('Code Clean');
 insert into tbl_livro (nome) values ('Engenharia de Software');
 insert into tbl_livro (nome) values ('Domain Driven Design');
+
+
+## O comando LIKE é usado junto com where substituindo 
+## o =, >, <, != ... e serve para encontrar resultados 
+## semelhantes ao desejado. O % indica a substituição alter
+## de algum termo. Por exemplo para pesquisar "livro legal"
+## é digitado "Livro%" ou "%legal"
+select * from tbl_livro where nome like '%code%';
